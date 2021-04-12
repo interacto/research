76a914079e5cf2d9fca4208cb796b5d417ef5ae7
arnobl <1052622+arnobl@users.noreply.github.com>
2021-04-12 22:17:20 +0200

    update

--- RQ2/performance/script.R
+++ RQ2/performance/script.R
@@ -1,49 +1,50 @@
 library(lsr) 
 
-widgetbinding <- rbind(read.table("data/Test_Results-widgetbinding1", head=FALSE), 
-                       read.table("data/Test_Results-widgetbinding2", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding3", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding4", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding5", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding6", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding7", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding8", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding9", head=FALSE),
-                       read.table("data/Test_Results-widgetbinding10", head=FALSE))
-listener <- rbind(read.table("data/Test_Results-listener1", head=FALSE),
-                  read.table("data/Test_Results-listener2", head=FALSE),
-                  read.table("data/Test_Results-listener3", head=FALSE),
-                  read.table("data/Test_Results-listener4", head=FALSE),
-                  read.table("data/Test_Results-listener5", head=FALSE),
-                  read.table("data/Test_Results-listener6", head=FALSE),
-                  read.table("data/Test_Results-listener7", head=FALSE),
-                  read.table("data/Test_Results-listener8", head=FALSE),
-                  read.table("data/Test_Results-listener9", head=FALSE),
-                  read.table("data/Test_Results-listener10", head=FALSE))
-rp <- rbind(read.table("data/Test_Results-RP1", head=FALSE), 
-            read.table("data/Test_Results-RP2", head=FALSE),
-            read.table("data/Test_Results-RP3", head=FALSE),
-            read.table("data/Test_Results-RP4", head=FALSE),
-            read.table("data/Test_Results-RP5", head=FALSE),
-            read.table("data/Test_Results-RP6", head=FALSE),
-            read.table("data/Test_Results-RP7", head=FALSE),
-            read.table("data/Test_Results-RP8", head=FALSE),
-            read.table("data/Test_Results-RP9", head=FALSE),
-            read.table("data/Test_Results-RP10", head=FALSE))
+widgetbinding_vals <- ((read.table("data/Test_Results-widgetbinding1", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding2", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding3", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding4", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding5", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding6", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding7", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding8", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding9", head=FALSE)/1000)+
+  (read.table("data/Test_Results-widgetbinding10", head=FALSE)/1000))/10;
+
+listener_vals <- ((read.table("data/Test_Results-listener1", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener2", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener3", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener4", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener5", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener6", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener7", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener8", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener9", head=FALSE)/1000)+
+  (read.table("data/Test_Results-listener10", head=FALSE)/1000))/10;
+
+# The 71 first values refer to test that involve comlex user interactions
+listener_vals_complex <- listener_vals[1:71,]
+widgetbinding_vals_complex <- widgetbinding_vals[1:71,]
+
+shapiro.test(listener_vals$V1) # p-value < 2.2e-16
+shapiro.test(widgetbinding_vals$V1) # p-value < 2.2e-16
+
+wilcox.test(widgetbinding_vals$V1, listener_vals$V1) # p-value = 0.9472
+
+mean(widgetbinding_vals$V1) # 1.334202
+mean(listener_vals$V1) # 1.331998
 
 svg("times.svg")
-boxplot(listener$V1, widgetbinding$V1, rp$V1, names = c("Callback", "Widget Binding", "RP"), ylab="Execution time (ms)", xlab="")
+boxplot(listener_vals$V1, widgetbinding_vals$V1, names = c("Callback", "Interacto"), ylab="Execution time (s)", xlab="", cex.lab=1.5, cex.axis=1.5)
 dev.off()
 
-mean(listener$V1) # 1331.998, 1.331 s
-mean(widgetbinding$V1) # 1334.202, 1.334 s
-mean(rp$V1) # 1329.621, 1.329 s
+# ------
+wilcox.test(widgetbinding_vals_complex, listener_vals_complex) # p-value = 0.9902
 
-cohensD(listener$V1, widgetbinding$V1) #  0.003255322
-cohensD(rp$V1, widgetbinding$V1) # 0.006777315
+mean(widgetbinding_vals_complex) # 1.383725
+mean(listener_vals_complex) # 1.381321
 
-shapiro.test(listener$V1) # p-value < 2.2e-16
-shapiro.test(widgetbinding$V1) # p-value < 2.2e-16
+boxplot(listener_vals_complex, widgetbinding_vals_complex, names = c("Callback", "Interacto"), ylab="Execution time (s)", xlab="", cex.lab=1.5, cex.axis=1.5)
 
-wilcox.test(listener$V1, widgetbinding$V1) #  0.8509
-wilcox.test(rp$V1, widgetbinding$V1) # 0.7282
+
+
